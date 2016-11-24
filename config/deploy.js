75cc756da8883642d8a4f25773ed89c84c96ddc3
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2016-11-24 09:14:37 +0100

    Added deploy

--- config/deploy.js
+++ config/deploy.js
@@ -1,28 +1,28 @@
-/* jshint node: true */
+/* eslint node: true */
 
 module.exports = function(deployTarget) {
   var ENV = {
-    build: {}
-    // include other plugin configuration that applies to all deploy targets here
-  };
-
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
+    build: {
+      environment: deployTarget
+    },
+    'revision-data': {
+      type: 'file-hash',
+      scm: null
+    },
+    'ssh-index': {
+      remoteDir: "/var/www/saechsi",
+      username: "root",
+      host: "saechsi.ch",
+      privateKeyFile: "~/.ssh/id_rsa",
+      allowOverwrite: true
+    },
+    rsync: {
+      dest: "/var/www/saechsi",
+      host: "root@saechsi.ch",
+      privateKey: "~/.ssh/id_rsa",
+      delete: false
+    }
   }
 
-  if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
-  }
-
-  if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
-  }
-
-  // Note: if you need to build some configuration asynchronously, you can return
-  // a promise that resolves with the ENV object instead of returning the
-  // ENV object synchronously.
-  return ENV;
-};
+  return ENV
+}
